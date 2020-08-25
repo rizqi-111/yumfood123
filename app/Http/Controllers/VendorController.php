ecb31dd7b75b98de42e00a5b3f9ae9a3dda9f897
Mac <mac@Macs-MacBook-Pro.local>
2020-08-25 22:19:55 +0700

    CRUD vendors

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\VendorResource;
 use App\Vendor;
+use App\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -25,9 +26,38 @@ class VendorController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) //Time : 10 Minutes
     {
         //
+        $request->validate([
+            'name' => 'required|max:128',
+            'logo' => 'required',
+            'tag' => 'required'
+        ]);
+
+        $vendor = new Vendor;
+
+        $vendor->name=$request['name'];
+        $vendor->logo=$request['logo'];
+        
+        $tags = explode(',',$request['tag']);
+        
+        $tag_ids = [];
+        foreach($tags as $t_name){
+            $tag = Tag::firstOrCreate(['name' => $t_name]);
+            $tag_ids[] = $tag->id;
+        }
+
+        $success=$vendor->save();
+
+        $vendor->tags()->sync($tag_ids);
+    
+        if(!$success)
+        {
+            return "gagal masuk";
+        }
+    
+        return "berhasil masuk";
     }
 
     /**
@@ -36,9 +66,10 @@ class VendorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id) //Time : 5 Minutes
     {
         //
+        return new VendorResource(Vendor::findOrFail($id)); 
     }
 
     /**
@@ -48,9 +79,36 @@ class VendorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id) //Time : 20 Menit
     {
         //
+        $request->validate([
+            'name' => 'required|max:128',
+            'logo' => 'required'
+        ]);
+
+        $tags = explode(',',$request['tag']);
+        
+        $tag_ids = [];
+        foreach($tags as $t_name){
+            $tag = Tag::firstOrCreate(['name' => $t_name]);
+            $tag_ids[] = $tag->id;
+        }
+
+        $vendor=Vendor::find($id);
+        $vendor->name=$request['name'];
+        $vendor->logo=$request['logo'];
+        $vendor->save();
+        
+        $success=$vendor->tags()->sync($tag_ids);
+    
+        if(!$success)
+        {
+            return "gagal ubah";
+        }
+    
+        return "berhasil ubah";
+
     }
 
     /**
@@ -59,8 +117,24 @@ class VendorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id)//Time : 10 menit
     {
         //
+        $vendor=Vendor::find($id);
+        if(is_null($vendor))
+        {
+            return "Tidak Ditemukan";
+        }
+    
+        $success=$vendor->delete();
+
+        $success = $vendor->tags()->detach();
+
+        if(!$success)
+        {
+            return "Berhasil Hapus Vendor, Tidak Ditemukan Taggable";
+        }
+    
+        return "Berhasil Hapus";
     }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    protected $fillable = ['name'];
     public function vendors()
     {
         return $this->morphedByMany('App\Vendor', 'taggable');
